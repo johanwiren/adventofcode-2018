c97ad4d4f5e6f9821a29679f3b7d6cec8c619fb6
Johan Wirén <johan@habity.co>
2019-01-11 14:32:06 +0100

    Rewrite of overly complicated solution for part 2

--- src/aoc/day_1.clj
+++ src/aoc/day_1.clj
@@ -1,8 +1,6 @@
 (ns aoc.day-1
   (:require [clojure.string :as str]
-            [clojure.java.io :as io])
-  (:import (com.google.common.hash BloomFilter
-                                   Funnels)))
+            [clojure.java.io :as io]))
 
 (defn input []
   (-> "aoc-day-1.txt"
@@ -12,27 +10,14 @@
       str/trim-newline))
 
 (defn p1-solution []
-  (reduce +
-          (map #(Integer/parseInt %) (str/split (input) #" "))))
-
-(defn p2-solution-list-and-bloom-impl []
-  (let [changes (map #(Long/parseLong %)
-                     (str/split (input) #" "))
-        bloom-filter (BloomFilter/create (Funnels/longFunnel) 1000000)]
-    (reduce (fn [fs change]
-              (let [f (+ (first fs) change)]
-                (if (.mightContain bloom-filter f)
-                  (do
-                    (println "Entry might be in bloom filter: " f)
-                    (when (some #{f} fs)
-                      (do
-                        (println "Solution found: " f)
-                        (reduced f))))
-                  (do
-                    (.put bloom-filter f)
-                    (cons f fs)))))
-            '(0)
-            (flatten (repeat changes)))))
+  (reduce + (map clojure.edn/read-string (str/split (input) #" "))))
 
 (defn p2-solution []
-  (p2-solution-list-and-bloom-impl))
+  (let [changes (->> (str/split (input) #" ")
+                     (map clojure.edn/read-string))]
+    (loop [f 0
+           fs #{}
+           [change & more] (flatten (repeat changes))]
+      (if (fs f)
+        f
+        (recur (+ f change) (conj fs f) more)))))
